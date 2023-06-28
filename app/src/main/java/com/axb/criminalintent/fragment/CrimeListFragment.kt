380794af65e3b01923e42cef9c6247d9c2371309
Michael_0619 <any_leon@163.com>
2023-06-28 09:16:19 +0800

    [menu]add add crime menu

--- app/src/main/java/com/axb/criminalintent/fragment/CrimeListFragment.kt
+++ app/src/main/java/com/axb/criminalintent/fragment/CrimeListFragment.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -74,6 +77,37 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    /*
+    * 接收选项菜单函数回调
+    * */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    /*
+    * 添加菜单
+    * */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
     /*
