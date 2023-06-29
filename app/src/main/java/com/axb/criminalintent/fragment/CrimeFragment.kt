300c2ee4f09afe5ed3875b30806f817c05a97ea0
Michael_0619 <any_leon@163.com>
2023-06-29 11:25:48 +0800

    [contact]Obtain contact name

--- app/src/main/java/com/axb/criminalintent/fragment/CrimeFragment.kt
+++ app/src/main/java/com/axb/criminalintent/fragment/CrimeFragment.kt
@@ -1,6 +1,8 @@
 package com.axb.criminalintent.fragment
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.text.Editable
@@ -181,6 +183,35 @@ class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
 
         if (crime.suspect.isNotEmpty()) {
             suspectButton.text = crime.suspect
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when {
+            resultCode != Activity.RESULT_OK -> return
+            requestCode == REQUEST_CONTACT && data != null -> {
+                val contactUri: Uri? = data.data
+                // Specify which fields you want your query to return values for
+                val queryFields = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+                // Perform your query - the contactUri is like a "where" clause here
+                val cursor = contactUri?.let {
+                    requireActivity().contentResolver
+                        .query(it, queryFields, null, null, null)
+                }
+                cursor?.use {
+                // Verify cursor contains at least one result
+                    if (it.count == 0) {
+                        return
+                    } // Pull out the first column of the first row of data -
+                    // that is your suspect's name
+                    it.moveToFirst()
+                    val suspect = it.getString(0)
+                    crime.suspect = suspect
+                    crimeDetailViewModel.saveCrime(crime)
+                    suspectButton.text = suspect
+                }
+            }
         }
     }
 
